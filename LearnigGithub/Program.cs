7a82d91ba2dd6308039e4810fc9025bf6e8d00f8
Taim Daboul <taimsssc2007@hotmail.com>
2025-04-11 22:52:44 +0300

    Added a new way of testing

--- LearnigGithub/Program.cs
+++ LearnigGithub/Program.cs
@@ -9,6 +9,11 @@ namespace LearnigGithub
             Console.WriteLine("Hello World!");
             Console.ReadLine();
 
+            float test = 100;
+            for (int i = 0; i < 1000; i++)
+            {
+                test -= .02f;
+            }
         }
     }
 }
